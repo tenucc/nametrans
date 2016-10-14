a075dbf05eca37952801a5f1938cfed50da04430
guwenyu2007 <guwenyu2007@126.com>
2016-10-14 16:53:26 +0800

    v1.0

--- 分类/svm.py
+++ 分类/svm.py
@@ -1,13 +1,13 @@
 from libsvm.python.svmutil import *
 from libsvm.python.svm import *
 
-y, x = svm_read_problem('pcpDistanceTrain.txt')  # 训练数据
+y, x = svm_read_problem('allTrain2.txt')  # 训练数据
 prob = svm_problem(y, x)
 #param = svm_parameter('-s 1 -t 3 -c 4')
 param = svm_parameter('-t 0 -c 4 -b 0')
 model = svm_train(prob, param)     # 训练好的模型
 svm_save_model('model_file', model)
 
-yt, xt = svm_read_problem('pcpDistanceTest.txt')
+yt, xt = svm_read_problem('allTest2.txt')
 p_label, p_acc, p_val = svm_predict(yt, xt, model)
 print(p_label)
